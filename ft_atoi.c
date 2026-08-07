06659e400aeeaaddb629f10a3a8f7fb5b4851742
junhonglim49791 <limjunhong4979@gmail.com>
2026-08-07 22:09:35 +0800

    ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junlim <junlim@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/07 20:44:48 by junlim            #+#    #+#             */
+/*   Updated: 2026/08/07 22:09:16 by junlim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isdigit(int c);
+/*
+	checks for white-space characters.  In the "C" and "POSIX"
+	locales, these are: space, form-feed ('\f'), newline
+	('\n'), carriage return ('\r'), horizontal tab ('\t'), and
+	vertical tab ('\v').
+*/
+
+int	ft_isspace(int c)
+{
+	if ((9 <= c && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *s)
+{
+	int	i;
+	int	sign;
+	int	sum;
+
+	i = 0;
+	sign = 1;
+	sum = 0;
+	while (ft_isspace(s[i]))
+		i++;
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(s[i]))
+		sum = sum * 10 + s[i++] - 48;
+	return (sign * sum);
+}
+
+/*
+#include <stdlib.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*nb = "\0";
+
+	printf("test case 0\n");
+	printf("Given num in string: %s\n", nb);
+	printf("ft_atoi: %d\n", ft_atoi(nb));
+	printf("atoi: %d\n", atoi(nb));
+	printf("-------------------------------------------\n");
+	
+	{
+		char *string_num = "  \t\n 1234";
+ 
+		printf("test case 1\n");
+		printf("give num in string: %s\n", string_num);
+		printf("ft_atoi: %d\n", ft_atoi(string_num));
+		printf("atoi: %d\n", atoi(string_num));
+		printf("-------------------------------------------\n");
+	}
+ 
+	{
+		char *string_num = "   +-";
+ 
+		printf("test case 3\n");
+		printf("give num in string: %s\n", string_num);
+		printf("ft_atoi: %d\n", ft_atoi(string_num));
+		printf("atoi: %d\n", atoi(string_num));
+		printf("-------------------------------------------\n");
+	}
+ 
+	{
+		char *string_num = "21474836499";
+ 
+		printf("test case 4\n");
+		printf("give num in string: %s\n", string_num);
+		printf("ft_atoi: %d\n", ft_atoi(string_num));
+		printf("atoi: %d\n", atoi(string_num));
+		printf("-------------------------------------------\n");
+	}
+}
+*/
