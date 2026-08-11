7dbbe923dd1c1bc96219a67e7f59217eff81a375
junhonglim49791 <limjunhong4979@gmail.com>
2026-08-12 01:42:21 +0800

    ft_putnbr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junlim <junlim@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/12 01:15:31 by junlim            #+#    #+#             */
+/*   Updated: 2026/08/12 01:41:58 by junlim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_itoa(int n);
+void	ft_putstr_fd(char *s, int fd);
+/*
+whenever deal with functions that return address from heap:
+1. must return to prevent memory leak. 
+2. Also need to check whether is NULL to prevent crash
+*/
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	*nbr;
+
+	nbr = ft_itoa(n);
+	if (!nbr)
+		return ;
+	ft_putstr_fd(nbr, fd);
+	free(nbr);
+}
+
+/*
+#include <limits.h>
+
+int	main(void)
+{
+	ft_putnbr_fd(0, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(7, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(-7, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(12345, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(-12345, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(-1, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(1, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(INT_MAX, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(INT_MIN, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(INT_MIN + 1, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(1000000, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(-1000000, 1);
+	ft_putstr_fd("\n", 1);
+	return (0);
+}
+*/
